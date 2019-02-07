9c0746127d4d5433417e843c7d381280223d42ea
Anton Suprunchuk <antouhou@gmail.com>
2019-02-07 13:32:24 +0700

    Add bindings

--- bindings/bindings.cpp
+++ bindings/bindings.cpp
@@ -4,3 +4,12 @@
 
 #include "emscripten/bind.h"
 #include "../src/testclass.h"
+
+using namespace emscripten;
+using namespace testlib;
+
+EMSCRIPTEN_BINDINGS(libtest) {
+        class_<TestClass>("TestClass")
+                .function("serialize", &TestClass::Serialize)
+                .class_function("getStringFromInstance", &TestClass::FromBytes, allow_raw_pointers());
+};
